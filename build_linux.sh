a1dd3662f220a6eed46afb8dbcbb2d6b76615a8f
andyque <guanghui8827@126.com>
2014-08-19 05:47:36 -0700

    finish linux spine compile

---
+++ build_linux.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# in order to build 32 bit application on 64bit machine
+# we have to install the following packages
+# and also we should specify the CXX_FLAGS = m32
+sudo apt-get install libx32gcc-4.8-dev
+
+sudo apt-get install libc6-dev-i386
+
+sudo apt-get install lib32stdc++6
+
+sudo apt-get install g++-multilib
+
+rm -rf prebuilt
+
+cmake -DLINUX32=1 -DLINUX=1 .
+
+make
+
+mkdir -p prebuilt/32/
+cp lib/libSpine.a prebuilt/32/
+rm -rf lib
+
+cmake -DLINUX32=0 -DLINUX=1 .
+
+make
+
+
+mkdir -p prebuilt/64/
+cp lib/libSpine.a  prebuilt/64/
