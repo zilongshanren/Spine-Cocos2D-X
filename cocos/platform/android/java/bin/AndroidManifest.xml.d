ec38ca4384f25f75f19ee505c20965a093c37d39
andyque <guanghui8827@126.com>
2014-08-27 21:07:13 +0800

    add cocos2d-x

---
+++ cocos/platform/android/java/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/guanghui/cocos2d-x-v3/cocos/platform/android/java/bin/AndroidManifest.xml : \
+/Users/guanghui/cocos2d-x-v3/cocos/platform/android/java/AndroidManifest.xml \
---
+++ cocos/platform/android/java/bin/classes/R.java.d
@@ -0,0 +1,1 @@
+ : /Users/guanghui/cocos2d-x-v3/cocos/platform/android/java/bin/AndroidManifest.xml \
